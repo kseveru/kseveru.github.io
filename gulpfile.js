808c36779a8116c9c96a9fee8121b430ef0fcfe2
kseveru <kseveru@yandex.ru>
2018-03-20 22:43:51 +0300

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,33 @@
+'use strict';
+
 var gulp = require("gulp");
-//var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
+var del = require("del");
+var ghPages = require('gulp-gh-pages');
 
-//gulp.task("sass", function () {
-//  return gulp.src("./sass/**/*.scss")
-//    .pipe(sass())
-//    .pipe(gulp.dest("css"));
-//});
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "src/*.html",
+    "src/fonts/*.{woff,woff2}"
+    ], {base: "./src/"})
+    .pipe(gulp.dest("build"));
+});
 
 gulp.task("style", function () {
-  return gulp.src("css/style.css")
-    .pipe(plumber())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("css"))
+  return gulp.src("src/css/style.css")
+    .pipe(postcss([ autoprefixer() ]))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+});
+
+gulp.task("build", function (done) {
+  run("clean", "copy", "style", done);
 });
