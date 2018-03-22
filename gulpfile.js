70e33e100302c7852ffd43ba31daaf131c964b4b
kseveru <kseveru@yandex.ru>
2018-03-22 20:17:05 +0300

    Разделение проекта на assets и build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var gulp = require("gulp");
+var del = require("del");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
-var del = require("del");
+var imagemin = require("gulp-imagemin");
 var ghPages = require('gulp-gh-pages');
 
 gulp.task("clean", function () {
@@ -14,20 +15,31 @@ gulp.task("clean", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "src/*.html",
-    "src/fonts/*.{woff,woff2}"
-    ], {base: "./src/"})
+    "assets/*.html",
+    "assets/fonts/*.{woff,woff2}"
+    ], {base: "./assets/"})
     .pipe(gulp.dest("build"));
 });
 
 gulp.task("style", function () {
-  return gulp.src("src/css/style.css")
+  return gulp.src("assets/css/style.css")
     .pipe(postcss([ autoprefixer() ]))
     .pipe(minify())
-    .pipe(rename("style.min.css"))
+    /*.pipe(rename("style.min.css"))*/
     .pipe(gulp.dest("build/css"))
 });
 
-gulp.task("build", function (done) {
-  run("clean", "copy", "style", done);
+gulp.task("images", function () {
+  return gulp.src("assets/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+      ]))
+    .pipe(gulp.dest("build/img"));
 });
+
+gulp.task("default", gulp.series(
+  "clean", "copy",
+  gulp.parallel("style", "images"))
+);
