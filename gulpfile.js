75a6af0a118c508434502854f2de1230f5f04b30
kseveru <kseveru@yandex.ru>
2018-03-25 15:49:01 +0300

    Отформатирован галпфайл

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var concat = require('gulp-concat');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssmin = require('gulp-csso');
-//var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
 var browserSync = require("browser-sync").create();
 
@@ -41,27 +40,7 @@ gulp.task('style', function () {
     .pipe(concat('style.css'))
     .pipe(postcss([ autoprefixer() ]))
     .pipe(cssmin())
-    //.pipe(rename('style.min.css'))
     .pipe(gulp.dest('./css'))
-});
-
-gulp.task('watch', function() {
-  gulp.watch('assets/css/*.css', gulp.series('style'));
-  gulp.watch('assets/*.html', gulp.series('html'));
-  gulp.watch('assets/img/**/*.{png,jpg,svg}', gulp.series('image'));
-  //gulp.watch('assets/**/*.*', gulp.series('copy'));
-});
-
-gulp.task('serve', function() {
-  browserSync.init({
-    server: '.'
-  });
-
-  gulp.watch('assets/css/*.css', gulp.series('style'));
-  gulp.watch('assets/*.html', gulp.series('html'));
-  gulp.watch('assets/img/**/*.{png,jpg,svg}', gulp.series('image'));
-
-  browserSync.watch('./**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('build',
@@ -69,5 +48,12 @@ gulp.task('build',
   gulp.parallel('copy', 'image', 'html', 'style'))
 );
 
-//gulp.task('dev', gulp.parallel('watch', 'serve')
-//);
+gulp.task('dev', function() {
+  browserSync.init({
+    server: '.'
+  });
+  gulp.watch('assets/css/*.css', gulp.series('style'));
+  gulp.watch('assets/*.html', gulp.series('html'));
+  gulp.watch('assets/img/**/*.{png,jpg,svg}', gulp.series('image'));
+  browserSync.watch('./**/*.*').on('change', browserSync.reload);
+});
