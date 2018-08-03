319788a82a71fcaaf82c13aab93c97203db92fb2
kseveru <kseveru@yandex.ru>
2018-08-03 19:20:36 +0300

    Добавление скринов traffic-school

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@
 
 var gulp = require('gulp');
 var del = require('del');
+var svgmin = require('gulp-svgmin');
 var htmlmin = require('gulp-htmlmin');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssmin = require('gulp-csso');
-var imagemin = require('gulp-imagemin');
+// var imagemin = require('gulp-imagemin');
 var browserSync = require("browser-sync").create();
 
 gulp.task('clean', function () {
@@ -14,18 +15,24 @@ gulp.task('clean', function () {
 });
 
 gulp.task('copy', function () {
-  return gulp.src('assets/fonts/*.{woff,woff2}', {base: './assets/'})
+  return gulp.src('assets/**/*.{woff,woff2,png,jpg}', {base: './assets/'})
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('image', function () {
-  return gulp.src('assets/img/**/*.{png,jpg,svg}')
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-      ]))
-    .pipe(gulp.dest('./img'));
+// gulp.task('image', function () {
+//   return gulp.src('assets/img/**/*.{png,jpg,svg}')
+//     .pipe(imagemin([
+//       imagemin.optipng({optimizationLevel: 1}),
+//       imagemin.jpegtran({progressive: true}),
+//       imagemin.svgo()
+//       ]))
+//     .pipe(gulp.dest('./img'));
+// });
+
+gulp.task('svgmin', function () {
+    return gulp.src('assets/**/*.svg', {base: './assets/'})
+        .pipe(svgmin())
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('html', function() {
@@ -43,15 +50,16 @@ gulp.task('style', function () {
 
 gulp.task('default',
   gulp.series('clean',
-  gulp.parallel('copy', 'image', 'html', 'style'))
+  gulp.parallel('copy', 'svgmin', 'html', 'style'))
 );
 
 gulp.task('dev', function() {
   browserSync.init({
     server: '.'
   });
-  gulp.watch('assets/css/*.css', gulp.series('style'));
+  gulp.watch('assets/**/*.{woff,woff2,png,jpg}', gulp.series('copy'));
+  gulp.watch('assets/img/**/*.{svg}', gulp.series('svgmin'));
   gulp.watch('assets/*.html', gulp.series('html'));
-  gulp.watch('assets/img/**/*.{png,jpg,svg}', gulp.series('image'));
+  gulp.watch('assets/css/*.css', gulp.series('style'));
   browserSync.watch('./**/*.*').on('change', browserSync.reload);
 });
